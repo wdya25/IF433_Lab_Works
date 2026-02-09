7c8006cb8bb03ce6b5003c56280ffc3a4c9f6f2f
unknown <wya101376@gmail.com>
2026-02-09 18:27:13 +0700

    week01: (tugas 1) setup GameStore file

---
+++ src/oop_127376_WidyaAyuSafitri/week01/GameStore.kt
@@ -0,0 +1,9 @@
+package oop_127376_WidyaAyuSafitri.week01
+
+fun main () {
+    val gameTitle ="Dota 2"
+    val price = 750000
+    println("Game store: SteamKW")
+    println("Game: $gameTitle")
+    println("Price: $price")
+}
