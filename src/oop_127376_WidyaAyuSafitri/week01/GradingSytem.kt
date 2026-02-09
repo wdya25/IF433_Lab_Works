83fbd95ced6f6434348da985bfaa8aa5fcf9fa5b
unknown <wya101376@gmail.com>
2026-02-09 17:10:23 +0700

    week01: initial setup and procedural code

---
+++ src/oop_127376_WidyaAyuSafitri/week01/GradingSytem.kt
@@ -0,0 +1,8 @@
+package oop_127376_WidyaAyuSafitri.week01
+
+fun main() {
+    var name: String = "Widya Ayu Safitri"
+    var score: Int = 80
+
+    println("Nama:" + name + ", Nilai: " +score)
+ }
