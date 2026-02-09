3ce3bd7e9bdffb2afab688d8aee80aa4a40be610
unknown <wya101376@gmail.com>
2026-02-09 18:41:32 +0700

    week01: (tugas 1) implement discount logic

--- src/oop_127376_WidyaAyuSafitri/week01/GameStore.kt
+++ src/oop_127376_WidyaAyuSafitri/week01/GameStore.kt
@@ -3,7 +3,11 @@ package oop_127376_WidyaAyuSafitri.week01
 fun main () {
     val gameTitle ="Dota 2"
     val price = 750000
+    val finalPrice = calculateDiscount(price)
     println("Game store: SteamKW")
     println("Game: $gameTitle")
     println("Price: $price")
+    println("Final Price: $finalPrice")
 }
+
+fun calculateDiscount(price: Int) = if (price > 500000) price - (price * 20/100) else price - (price * 10 / 100)
