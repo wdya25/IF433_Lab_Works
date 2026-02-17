2f702c0b0654736f9bbd0c17f7866bff6fd697e7
unknown <wya101376@gmail.com>
2026-02-17 23:15:35 +0700

    week02: challenge refactor primary constructor using default args

--- src/oop_127376_WidyaAyuSafitri/week02/Student.kt
+++ src/oop_127376_WidyaAyuSafitri/week02/Student.kt
@@ -1,6 +1,6 @@
 package oop_127376_WidyaAyuSafitri.week02
 
-class Student (val name: String, val nim: String, var major: String) {
+class Student (val name: String, val nim: String,var gpa: Double = 0.0, var major: String ) {
     init {
         if (nim.length != 5) {
             println("WARNING: Objek tercipta dengan NIM ($nim) yang tidak valid!")
