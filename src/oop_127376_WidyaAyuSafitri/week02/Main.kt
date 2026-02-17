8a76dce5c97f50c432f88646777bf0024ef28765
unknown <wya101376@gmail.com>
2026-02-17 23:39:49 +0700

    week02: implement selection logic for constructors

--- src/oop_127376_WidyaAyuSafitri/week02/Main.kt
+++ src/oop_127376_WidyaAyuSafitri/week02/Main.kt
@@ -24,4 +24,21 @@ fun main() {
         val s1 = Student(name, nim, major)
         println("Status: Pendaftaran Selesai.")
     }
+
+    print("Pilih Jalur (1. Reguler, 2. Umum): ")
+    val type = scanner.nextInt()
+    scanner.nextLine()
+
+    if (type == 1) {
+        print("Masukkan Jurusan: ")
+        val major = scanner.nextLine()
+        val s1 = Student(name, nim, major)
+        println("Terdaftar di: ${s1.major} dengan GPA awal ${s1.gpa}")
+    } else if (type == 2) {
+        val s2 = Student(name, nim)
+        println("Terdaftar di: ${s2.major} dengan GPA awal ${s2.gpa}")
+    } else {
+        println("Pilihan ngawur, pendaftaran batal!")
+    }
+
 }
