073ab1641b9e74d6dfad52f92677b518e84d677e
unknown <wya101376@gmail.com>
2026-02-17 23:57:29 +0700

    week02: (task 1) implement library input system

--- src/oop_127376_WidyaAyuSafitri/week02/Main.kt
+++ src/oop_127376_WidyaAyuSafitri/week02/Main.kt
@@ -41,4 +41,33 @@ fun main() {
         println("Pilihan ngawur, pendaftaran batal!")
     }
 
+    println("\n" + "=".repeat(50))
+    libraryFineSystem()
+}
+fun libraryFineSystem() {
+    val scanner = Scanner(System.`in`)
+
+    println("---SISTEM PEMINJAMAN BUKU ---")
+
+    print("Masukkan Judul Buku: ")
+    val title = scanner.nextLine()
+
+    print("Masukkan Nama Peminjam: ")
+    val borrower = scanner.nextLine()
+
+    print("Masukkan Lama Pinjam (hari): ")
+    var duration = scanner.nextInt()
+
+    if (duration < 0) {
+        println("Lama pinjam tidak valid (minus)! Otomatis diubah menjadi 1 hari.")
+        duration = 1
+    }
+
+    val loan = Loan(title, borrower, duration)
+
+    println("\n-- DETAIL PEMINJAMAN ---")
+    println("Judul Buku: ${loan.bookTitle}")
+    println("Peminjam: ${loan.borrower}")
+    println("Lama Pinjam: ${loan.loanDuration} hari")
+    println("Total Denda: Rp ${loan.calculateFine()}")
 }
