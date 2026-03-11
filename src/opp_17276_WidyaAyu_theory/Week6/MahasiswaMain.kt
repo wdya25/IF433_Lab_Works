95823358328324d5e733346a0d98970bb3f716cf
unknown <wya101376@gmail.com>
2026-03-11 13:22:49 +0700

    awal

---
+++ src/opp_17276_WidyaAyu_theory/Week6/MahasiswaMain.kt
@@ -0,0 +1,6 @@
+package opp_17276_WidyaAyu_theory.Week6
+
+fun main(){
+    val org = Organisasi();
+    org.rapat();
+}
---
+++ src/opp_17276_WidyaAyu_theory/Week6/Organisasi.kt
@@ -0,0 +1,9 @@
+package opp_17276_WidyaAyu_theory.Week6
+
+class Organisasi {
+    val namaOrg: String = "GCD";
+
+    fun rapat(){
+        println("Rapatin buat game ");
+    }
+}
