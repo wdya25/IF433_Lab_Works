d71349186cd3c5f99e7da4aed44bdb80e26af163
unknown <wya101376@gmail.com>
2026-03-25 16:07:27 +0700

    week07: (task) create GameItem data class

--- src/oop_127376_WidyaAyuSafitri/Week07/GameModels.kt
+++ src/oop_127376_WidyaAyuSafitri/Week07/GameModels.kt
@@ -1,4 +1,14 @@
 package oop_127376_WidyaAyuSafitri.Week07
 
-enum class GameModels {
+enum class ItemRarity (val dropChance: Int) {
+    COMMON(70),
+    UNCOMMON(50),
+    RARE(30),
+    EPIC(10),
+    LEGENDARY(1);
 }
+data class Item(
+    val name: String,
+    val damage: Int,
+    val rarity: ItemRarity
+)
