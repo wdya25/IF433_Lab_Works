688879f805fea32d34c3a33b56d251ee73b57083
unknown <wya101376@gmail.com>
2026-04-15 16:35:46 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_127376_WidyaAyuSafitri/Week08/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_127376_WidyaAyuSafitri.Week08;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId) {
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
