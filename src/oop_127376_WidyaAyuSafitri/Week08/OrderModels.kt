6dc683941e6a8fdbcb81c94528411c3a6f5c9829
unknown <wya101376@gmail.com>
2026-04-15 15:05:25 +0700

    week08: create nested classes for order

---
+++ src/oop_127376_WidyaAyuSafitri/Week08/OrderModels.kt
@@ -0,0 +1,6 @@
+package oop_127376_WidyaAyuSafitri.Week08
+
+class City(val name: String)
+class Address(val city: City?)
+class DeliveryDetails(val address: Address?)
+class Order(val deliveryDetails: DeliveryDetails?, val totalPrice: Int?)
