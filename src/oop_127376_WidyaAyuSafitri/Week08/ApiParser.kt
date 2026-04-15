9d28cd6999906fbb9eb677af44dba2852dea15c9
unknown <wya101376@gmail.com>
2026-04-15 16:41:12 +0700

    week08: (task) implement payment processing with !!

--- src/oop_127376_WidyaAyuSafitri/Week08/ApiParser.kt
+++ src/oop_127376_WidyaAyuSafitri/Week08/ApiParser.kt
@@ -24,4 +24,12 @@ class ApiParser {
             else -> null
         }
     }
+    fun checkout(product: Product) {
+        val productId = when (product) {
+            is Electronic -> product.id
+            is Clothing   -> product.id
+        }
+        val transactionId = JavaPaymentService.processPayment(productId)!!
+        println("Pembayaran berhasil! Transaction ID: $transactionId")
+    }
 }
