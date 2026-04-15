45dbfa59af4b550062b2c0636cd7485539ba7358
unknown <wya101376@gmail.com>
2026-04-15 15:35:23 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_127376_WidyaAyuSafitri/Week08/Main.kt
+++ src/oop_127376_WidyaAyuSafitri/Week08/Main.kt
@@ -23,4 +23,10 @@ fun main (){
         "Laptop",
         4500000.0
     )
+    for (item in mixedData) {
+        val text: item as? String
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
