70daa7cd1b6f321660c01b96deddcc43806a7a55
unknown <wya101376@gmail.com>
2026-04-15 13:50:17 +0700

    belajar materi null(safe?. safe+.let+elvis)

--- src/opp_17276_WidyaAyu_theory/Week8/SafeElvis.kt
+++ src/opp_17276_WidyaAyu_theory/Week8/SafeElvis.kt
@@ -7,7 +7,9 @@ fun main () {
     val almt = Alamat(nomor = 47, kota = "Tangerang");
     val mhs = Mahasiswa(nim = "12345", addr = almt);
 
-    val defaulAlamat = mhs.addr?:"Gak jelas , ga tau dimana "
+    val defaulAlamat = mhs.addr?.let { //letnya disini
+       alamat -> "sini nih... ${mhs.addr.kota} nomornya ${mhs.addr.nomor}";
+    }?: "Gak jelas , ga tau dimana "
 
     println("Nimnya ${mhs.nim} tinggalnya di ${defaulAlamat}") // disinipakai ganti
 }
