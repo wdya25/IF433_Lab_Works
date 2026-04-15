263c1f6956429677ec91be2675e389e8c8340f71
unknown <wya101376@gmail.com>
2026-04-15 14:05:58 +0700

    belajar materi not null (safty net null *itu typo filenya )

--- src/opp_17276_WidyaAyu_theory/Week8/SafeNetNull.kt
+++ src/opp_17276_WidyaAyu_theory/Week8/SafeNetNull.kt
@@ -1,11 +1,11 @@
 package opp_17276_WidyaAyu_theory.Week8
 
 fun main (){
-    var nama:String= "soobin";
+    var nama:String?= null;
 
     try {
-        println("nama kamu ${nama.uppercase()}");
-    } catch(ex:Exception) {
+        println("nama kamu ${nama!!.uppercase()}");
+    } catch(ex:Exception) { //sama aja kalau pakai NullPointerexception (handel hanya null sedangkan itu dia menghandel eror keseluruhan )
         println("errornya adalah  ${ex.message}");
     }
 }
