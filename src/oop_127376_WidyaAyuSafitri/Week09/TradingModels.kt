c36c4cc249a8ba0310e2f96844041d768af1148a
unknown <wya101376@gmail.com>
2026-04-22 15:48:03 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_127376_WidyaAyuSafitri/Week09/TradingModels.kt
@@ -0,0 +1,9 @@
+package oop_127376_WidyaAyuSafitri.Week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
