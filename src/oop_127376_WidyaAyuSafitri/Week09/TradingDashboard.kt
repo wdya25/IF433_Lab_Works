a58d70fe45a37d195882a87955507387966e6171
unknown <wya101376@gmail.com>
2026-04-22 16:00:44 +0700

    week09: (task) extract winning trades using filter

--- src/oop_127376_WidyaAyuSafitri/Week09/TradingDashboard.kt
+++ src/oop_127376_WidyaAyuSafitri/Week09/TradingDashboard.kt
@@ -10,4 +10,5 @@ fun main(){
         TradeLog("BNBUSDT", "SHORT", 5, -1.0, "CLOSED")
     )
     val closedTrades = tradeHistory.filter { it.status == "CLOSED" }
+    val winningTrades = closedTrades.filter { it.roe > 0 }
 }
