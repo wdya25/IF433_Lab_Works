93431491179f67b91f500dc2d9e2ada4031dc895
unknown <wya101376@gmail.com>
2026-04-29 15:57:08 +0700

    week10: (task) print dashboard using forEach on generic list

--- src/oop_127376_WidyaAyuSafitri/Week10/CryptoDashboard.kt
+++ src/oop_127376_WidyaAyuSafitri/Week10/CryptoDashboard.kt
@@ -8,12 +8,14 @@ fun main() {
     coinRepo.add(Coin("USDT", 1250.0))
     println("Coin Repository berhasil diinisialisasi!")
     println("Jumlah koin saat ini: ${coinRepo.getAll().size}")
-
     val response = ApiResponse("200 OK", coinRepo.getAll())
+
     println("\n=== API Response ===")
     println("Status : ${response.status}")
+    println("Total Koin: ${response.data.size}")
     println("Data yang diterima:")
     response.data.forEach { coin ->
         println("• ${coin.name} : ${coin.balance}")
     }
+    println("Dashboard berhasil ditampilkan menggunakan generic list!")
 }
