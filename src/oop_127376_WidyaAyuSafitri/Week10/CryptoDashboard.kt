3efd90ab83e0251f2e823e03c3e246653751a53a
unknown <wya101376@gmail.com>
2026-04-29 15:45:47 +0700

    week10: (task) populate coin repository

--- src/oop_127376_WidyaAyuSafitri/Week10/CryptoDashboard.kt
+++ src/oop_127376_WidyaAyuSafitri/Week10/CryptoDashboard.kt
@@ -3,6 +3,14 @@ package oop_127376_WidyaAyuSafitri.Week10
 fun main() {
     println("=== CRYPTO WALLET DASHBOARD ===")
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 2.45))
+    coinRepo.add(Coin("ETH", 15.7))
+    coinRepo.add(Coin("USDT", 1250.0))
     println("Coin Repository berhasil diinisialisasi!")
     println("Jumlah koin saat ini: ${coinRepo.getAll().size}")
+
+    println("\nDaftar Koin di Wallet:")
+    coinRepo.getAll().forEach { coin ->
+        println("• ${coin.name} : ${coin.balance}")
+    }
 }
