0c71a6e340ff53768d4d451a79512d8510091616
unknown <wya101376@gmail.com>
2026-04-29 15:07:11 +0700

    week10: implement PairBox in main function

--- src/oop_127376_WidyaAyuSafitri/Week10/MultiBox.kt
+++ src/oop_127376_WidyaAyuSafitri/Week10/MultiBox.kt
@@ -1,3 +1,9 @@
 package oop_127376_WidyaAyuSafitri.Week10
 
 class PairBox<K, V>(var key: K, val Value: V)
+
+fun main (){
+    println("\n===== TEST MULTIPLE PARAMETER ====")
+    val itemPrice = PairBox("Bitcoin", 65000)
+    println("Asset : ${itemPrice.key}, harga: ${itemPrice.Value} USD")
+}
