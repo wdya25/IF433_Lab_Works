77fb5291d68079131d79af010fa7f40501c14f4a
unknown <wya101376@gmail.com>
2026-04-29 15:15:53 +0700

    week10: test generic functions with Double and String

--- src/oop_127376_WidyaAyuSafitri/Week10/Main.kt
+++ src/oop_127376_WidyaAyuSafitri/Week10/Main.kt
@@ -1,6 +1,11 @@
 package oop_127376_WidyaAyuSafitri.Week10
 
 fun main (){
+    println("\n===== TEST GENERIC FUNTIONS  ====")
+    printData(3.14)
+    val result = processData ("Stable coin")
+    printData("Hasil process: $result")
+
     println("===== TEST GENERIC Class ====")
     val intBox = Box(100)
     val stringBox = Box("Generic in kotlin")
