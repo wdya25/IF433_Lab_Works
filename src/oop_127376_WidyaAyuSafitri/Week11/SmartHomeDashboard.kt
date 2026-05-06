94b64eed234e4d084c558c84e8eefb582d3b34c1
unknown <wya101376@gmail.com>
2026-05-07 01:36:14 +0700

    week11: (task) configure Philips WiZ using apply

--- src/oop_127376_WidyaAyuSafitri/Week11/SmartHomeDashboard.kt
+++ src/oop_127376_WidyaAyuSafitri/Week11/SmartHomeDashboard.kt
@@ -4,5 +4,15 @@ fun main() {
     println("=== SMART HOME DASHBOARD ===")
     val homeDevices = mutableListOf<SmartDevice>()
     println("Sistem Smart Home telah diinisialisasi")
+    val livingRoomLight = SmartDevice(
+        name = "Philips WiZ Living Room",
+        category = "Lighting"
+    ).apply {
+        isOnline = true
+        powerLoad = 12
+    }.also {
+        homeDevices.add(it)
+    }
+    println("Lampu Philips WiZ berhasil dikonfigurasi dan ditambahkan")
     println("Jumlah perangkat saat ini: ${homeDevices.size}")
 }
