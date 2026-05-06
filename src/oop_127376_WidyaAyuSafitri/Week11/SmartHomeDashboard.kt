86a98e83e3a488f997e00a0c556c66d98a02a06b
unknown <wya101376@gmail.com>
2026-05-07 01:32:17 +0700

    week11: (task) initialize smart home device list

---
+++ src/oop_127376_WidyaAyuSafitri/Week11/SmartHomeDashboard.kt
@@ -0,0 +1,8 @@
+package oop_127376_WidyaAyuSafitri.Week11
+
+fun main() {
+    println("=== SMART HOME DASHBOARD ===")
+    val homeDevices = mutableListOf<SmartDevice>()
+    println("Sistem Smart Home telah diinisialisasi")
+    println("Jumlah perangkat saat ini: ${homeDevices.size}")
+}
