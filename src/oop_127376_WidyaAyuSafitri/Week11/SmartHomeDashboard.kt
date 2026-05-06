46ec9e40f5cb73de5b6eed0a32b590887dc8bf5d
unknown <wya101376@gmail.com>
2026-05-07 01:42:09 +0700

    week11: (task) configure AC unit using run

--- src/oop_127376_WidyaAyuSafitri/Week11/SmartHomeDashboard.kt
+++ src/oop_127376_WidyaAyuSafitri/Week11/SmartHomeDashboard.kt
@@ -22,6 +22,18 @@ fun main() {
             println("(LOG) Kamera terhubung")
             homeDevices.add(it)
         }
+    val acUnit = "Daikin Inverter (Kabel 3x2.5)".run {
+        SmartDevice(
+            name = this,
+            category = "HVAC",
+            isOnline = false,
+            powerLoad = 800
+        )
+    }
+    homeDevices.add(acUnit)
+    val petFeeder = SmartDevice("Picolo's Auto Feeder", "Pet Care", true, 10)
+    homeDevices.add(petFeeder)
+    println(" AC dan Auto Feeder berhasil dikonfigurasi")
     println("Lampu Philips WiZ berhasil dikonfigurasi dan ditambahkan")
     println("Jumlah perangkat saat ini: ${homeDevices.size}")
 }
