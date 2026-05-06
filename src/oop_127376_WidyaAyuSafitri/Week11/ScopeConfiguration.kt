47127d0734ee0c6a7c3647e868ee7e80134f560e
unknown <wya101376@gmail.com>
2026-05-07 01:54:34 +0700

    week11: implement apply for object initialization(maaf kak lupa commit ini )

--- src/oop_127376_WidyaAyuSafitri/Week11/ScopeConfiguration.kt
+++ src/oop_127376_WidyaAyuSafitri/Week11/ScopeConfiguration.kt
@@ -3,7 +3,7 @@ package oop_127376_WidyaAyuSafitri.Week11
 data class User(var name: String = " ", var age: Int = 0)
 
 fun main(){
-    println("=== TEST APPLY ===")
+    println("=== TEST APPLY ===")// maaf kak lupa commit ini tadi
    val user = User().apply {
         name = "Alex"
         age = 25
