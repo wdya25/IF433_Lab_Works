098569cf077d2a4713d715615eaa57b0d8f43357
unknown <wya101376@gmail.com>
2026-05-07 00:56:57 +0700

    week11: test extension functions in main

---
+++ src/oop_127376_WidyaAyuSafitri/Week11/Main.kt
@@ -0,0 +1,10 @@
+package oop_127376_WidyaAyuSafitri.Week11
+
+fun main(){
+    println("=== TEST EXTENSION FUNCTIONS ===")
+    println("Alex".addGreeting())
+    println("Hi".repeatTimes(3))
+
+    val text: String? = null
+    println("Apkah null/empty? ${text.isNullOrEmptyCustom()}")
+}
