3f596897d731aa6c72f965ec7719bc52fd54b294
unknown <wya101376@gmail.com>
2026-05-13 13:34:38 -0300

    throw

--- src/opp_17276_WidyaAyu_theory/Week12/M12lat01.kt
+++ src/opp_17276_WidyaAyu_theory/Week12/M12lat01.kt
@@ -14,15 +14,26 @@ fun pembagian (pembilang: Int, penyebut: Int){
 //Exception - expression - tipe data
 fun cek_tipe_data(){
     var angka:Int = try {
-        Integer.parseInt( "adadssad") // ini harus kasih angka kalau pakai huruf egak
+        Integer.parseInt( "10") // ini harus kasih angka kalau pakai huruf egak
     } catch (e: Exception) {
         println("ada error di Cek tipe data ${e.message}")
-        111
+        111 // harus pakai angka jangan lupa
     }
     println("angka kamu adalah ${angka}")
 }
 
+//throw (untuk membuat eror)
+fun cek_saldo(saldoSaya: Int){
+    if (saldoSaya < 1000){
+        throw IllegalArgumentException("saldo $saldoSaya tidak perlu bayar pajak")
+    } else {
+        println("selamat kamu orang kaya ")
+    }
+}
+
 fun main (){
+    // coba throw
+    cek_saldo(saldoSaya = 1200) //kalau angka di bawah 1000 dia akan eror dan program akan crash
     //Exception -expression
     cek_tipe_data()// harus ada agar bisa jalan
     pembagian (pembilang = 10, penyebut = 0)
