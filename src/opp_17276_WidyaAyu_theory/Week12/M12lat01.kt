b90b9fd8c923569246c194f64c95b86e62fd1be1
unknown <wya101376@gmail.com>
2026-05-13 13:15:42 -0300

    week12 : trycatch after nya pakai trycatch)

--- src/opp_17276_WidyaAyu_theory/Week12/M12lat01.kt
+++ src/opp_17276_WidyaAyu_theory/Week12/M12lat01.kt
@@ -2,8 +2,15 @@ package opp_17276_WidyaAyu_theory.Week12
 
 //basic exception - tryCatch
 fun pembagian (pembilang: Int, penyebut: Int){
-    val hasilBagi =pembilang/penyebut
-    println("hasil bagi =$hasilBagi")
+    try {
+        val hasilBagi =pembilang/penyebut
+        println("hasil bagi = $hasilBagi")
+    } catch (e: Exception) {
+        println("error di pembagian: ${e.message}")
+    } finally { //finally mau dia eror atau tidak dia bakal di panggil
+        println("selesai pembagian")
+    }
+
 }
 fun main (){
     pembagian (pembilang = 10, penyebut = 0)
