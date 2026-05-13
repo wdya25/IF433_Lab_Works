944dee08924c6666c591c771c1a6505bb03ab547
unknown <wya101376@gmail.com>
2026-05-13 13:13:01 -0300

    week12 : trycatch before masih eror)

---
+++ src/opp_17276_WidyaAyu_theory/Week12/M12lat01.kt
@@ -0,0 +1,10 @@
+package opp_17276_WidyaAyu_theory.Week12
+
+//basic exception - tryCatch
+fun pembagian (pembilang: Int, penyebut: Int){
+    val hasilBagi =pembilang/penyebut
+    println("hasil bagi =$hasilBagi")
+}
+fun main (){
+    pembagian (pembilang = 10, penyebut = 0)
+}
