d8ad948ec6ab651dfbf7c0d5220ab6752bbbf1f1
unknown <wya101376@gmail.com>
2026-05-20 21:37:07 +0700

    week13: (task) create TradeRecord data class

---
+++ src/oop_127376_WidyaAyuSafitri/Week13/CryptoTradeLogger.kt
@@ -0,0 +1,9 @@
+package oop_127376_WidyaAyuSafitri.Week13
+
+data class TradeRecord(
+    val id: Int,
+    val symbol: String,
+    val type: String,
+    val margin: Double,
+    val pnl: Double
+)
