55322a890277125941149df2b4d873e34767a315
unknown <wya101376@gmail.com>
2026-05-24 23:38:29 -0300

    week14: create Database abstraction and implementations

---
+++ src/oop_127376_WidyaAyuSafitri/week14/DipRefactored.kt
@@ -0,0 +1,8 @@
+package oop_127376_WidyaAyuSafitri.week14
+
+interface Database {
+    fun query(sql: String): List<String>
+}
+class PostgresDatabase : Database {
+    override fun query(sql: String) = listOf("pg_data1", "pg_data2")
+}
