5ab07844e11150d436a14a83ff86497f8d3cc6d0
unknown <wya101376@gmail.com>
2026-05-24 22:48:34 -0300

    week14: setup project structure

--- src/opp_17276_WidyaAyu_theory/Week13/Weeek12Lt.kt
+++ src/opp_17276_WidyaAyu_theory/Week13/Weeek12Lt.kt
@@ -1,5 +1,9 @@
 package opp_17276_WidyaAyu_theory.Week13
+import java.io.BufferedReader
 import java.io.File
+import java.io.FileReader
+
+data class Mahasiswa(val nim: String, val nama: String, val deskripsi: String)
 
 fun main() {
     println("==== cara 1 baca isi file ====")
@@ -10,4 +14,35 @@ fun main() {
     println("===== CARA 2 baca isi file ======")
     val contentsaya =file1.readLines()
     contentsaya.forEach { println(it) }
+
+   println("\n\n=== Cara 3 Baca Isi File - BUFFERED READER ===")
+    val file2 = BufferedReader(FileReader("src/opp_17276_WidyaAyu_theory/Week13/contohfile.txt"))
+    var isiFile:String? = file2.readLine()
+    while(isiFile != null) {
+        print(isiFile)
+        isiFile = file2.readLine()
+    }
+    file2.close()
+
+    println("===== CARA 4 baca isi file - buffered reader  (ver use) ======")
+    val file3 = File("src/opp_17276_WidyaAyu_theory/Week13/contohfile.txt").bufferedReader()
+    file3.use{
+        it.lineSequence()
+            .filter{it.isNotEmpty()}
+            .forEach{println(it)}
+    }
+
+    println("\n==== CARA 1 buat file====")
+    val fileSaya= File("src/opp_17276_WidyaAyu_theory/Week13/contohfile.txt")
+    fileSaya.writeText("saya suka ke pasar \nBeli buah dan sayuran ")
+    fileSaya.appendText("\n untuk ayah dan ibu \nDan kakak adik")
+    val listBelanjaan = listOf("ayam", "pisang", "beras")
+    fileSaya.appendText("\n"+listBelanjaan.joinToString("\n"))
+    fileSaya.printWriter().use {
+        it.println("Saya kuliah di UMN")
+        it.println("UMN Kampus bagus")
+        it.println("Saya jurusan IF")
+    }
+
+
 }
